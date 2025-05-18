86f2fff7c8344d137cf311bb4767f11b3a3e03c3
ReemAli41 <reem.ali.abdelrahman@gmail.com>
2025-05-18 11:39:48 +0300

    Add ResponseDTO Class

---
+++ LDM_Mobile_Manager.Common/Entities/ResponseDTOs/ResponseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDM_Mobile_Manager.Common.Entities.ResponseDTOs
+{
+    public class ResponseDTO<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+        public ResponseDTO(bool isSuccess, string message, T data)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+            Data = data;
+        }
+    }
+}
