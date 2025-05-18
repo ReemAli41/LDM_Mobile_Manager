b890ad6de81c978368427a17de40a88e568498b3
HagarMekhemer <mekhemerhager@gmail.com>
2025-05-18 13:46:42 +0300

    //added TokenDSL

---
+++ LDM_Mobile_Manager.DataService/TokenDSL.cs
@@ -0,0 +1,29 @@
+﻿using LDM_Mobile_Manager.Common.Entities.RequestDTOs;
+using LDM_Mobile_Manager.Common.Entities.ResponseDTOs;
+using LDM_Mobile_Manager.LDM_Mobile_Manager.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LDM_Mobile_Manager.DSL
+{
+    public class TokenDSL
+    {
+        private readonly TokenManager _tokenManager;
+
+        public TokenDSL(TokenManager tokenManager)
+        {
+            _tokenManager = tokenManager;
+        }
+
+        public GenerateTokenResponseDTO GenerateToken(TokenCredentialsRequestDTO request)
+        {
+            return _tokenManager.GetToken(request);
+        }
+    }
+}
+
+
