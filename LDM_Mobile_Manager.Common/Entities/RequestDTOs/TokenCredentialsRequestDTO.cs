e1eb33882a7c56d26c93973cb76b405c4d4598ec
HagarMekhemer <mekhemerhager@gmail.com>
2025-05-18 11:20:10 +0300

    //Add Token Credentials Request

--- LDM_Mobile_Manager.Common/Entities/RequestDTOs/TokenCredentialsRequestDTO.cs
+++ LDM_Mobile_Manager.Common/Entities/RequestDTOs/TokenCredentialsRequestDTO.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LDM_Mobile_Manager.Common.Entities
 {
-    internal class TokenCredentialsRequestDTO
+    public class TokenCredentialsRequestDTO
     {
+        [Required(ErrorMessage = "The ClientCode field is required.")]
+        public string ClientCode { get; set; }
+
+        [Required(ErrorMessage = "The userName field is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "The password field is required.")]
+        public string Password { get; set; }
     }
 }
